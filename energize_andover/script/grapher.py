0c1e6f6c3b9d8d036c68fc106f9f96f7321749a5
ea <EnergizeAndover@gmail.com>
2016-06-28 14:52:35 -0400

    fixed graphing error so graphs show proper data

--- energize_andover/script/grapher.py
+++ energize_andover/script/grapher.py
@@ -21,7 +21,9 @@ def file_parser(file, field_to_graph='MAIN ELECTRIC METER.Analog Inputs.KW_Total
                 for date in index:
                     new_index.append(date[:13])
             df.index = new_index
-            df = df.groupby(df.index, sort=True).max()
+            Maxdf = df.groupby(df.index, sort=True).max()
+            Mindf = df.groupby(df.index, sort=True).min()
+            df = Maxdf - Mindf
     else:
         if not grouping == 'min':
             index = df.index
@@ -36,7 +38,7 @@ def file_parser(file, field_to_graph='MAIN ELECTRIC METER.Analog Inputs.KW_Total
                 for date in index:
                     new_index.append(date[:13])
             df.index = new_index
-            df = df.groupby(df.index, sort=True).sum()
+            df = df.groupby(df.index, sort=True).mean()
     graph = df[field_to_graph]
     if len(graph.index) > 20:
         graph.plot.bar(grid=True,
