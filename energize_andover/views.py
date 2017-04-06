d729d881b8224c7e155fef5a007b5142801bc6a7
EnergizeAndover <EnergizeAndover@gmail.com>
2017-04-06 15:55:39 -0400

    improved search

--- energize_andover/views.py
+++ energize_andover/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect, HttpRequest
 from django.shortcuts import render, get_object_or_404
-
 from .models import *
 import pandas as pd
 import requests
