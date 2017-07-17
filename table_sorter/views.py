e8afb75e0e113c303c0cb90fc7435dca80b72f40
Peter Audier <4peetah@gmail.com>
2017-07-17 13:20:27 -0400

    Created the tables for each data type
    # Conflicts:
    #	.idea/energize_andover.iml
    #	.idea/inspectionProfiles/Project_Default.xml
    #	.idea/misc.xml
    #	.idea/workspace.xml

--- table_sorter/views.py
+++ table_sorter/views.py
@@ -12,20 +12,23 @@ def list(request, school_id, type):
     school_obj = get_object_or_404(School, pk=school_id)
     if check_school_privilege(school_obj, request) is False:
         return HttpResponseRedirect("electric")
-    #if request.GET.get("Adder"):
+    # if request.GET.get("Adder"):
     #    return render(request, "energize_andover/Adder.html", {'school_choice': school_obj})
-    Closets = school_obj.closets().order_by('id')
-    Panels = school_obj.panels().order_by('id')
-    Rooms = school_obj.rooms().order_by('id')
+    if type == "closets":
+        model_obj = school_obj.closets().order_by('id')
+    elif type == "panels":
+        model_obj = school_obj.panels().order_by('id')
+    elif type == "rooms":
+        model_obj = school_obj.rooms().order_by('id')
+    else:
+        model_obj = school_obj.devices().order_by('id')
+
     form = SearchForm()
-    devices = school_obj.devices().order_by('id')
-    if len(devices) == 0:
-        devices = Device.objects.none()
+
     picture = "energize_andover/" + school_obj.Name + ".jpg"
     #if not os.path.isfile(picture):
     #    picture = None
-    return render(request, 'energize_andover/Test.html',
+    return render(request, 'energize_andover/TableSort.html',
                   {'title': 'Table Sorting', 'type': type, 'school': school_obj,
-                   'Rooms': Rooms, 'Panels': Panels, 'Closets': Closets, 'Devices': devices, 'picture': picture,
-                   'form': form})
+                   'model_obj': model_obj, 'form': form})
 
